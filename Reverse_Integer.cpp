a88b7a023e4282b7d73b3f96269d724399c42f95
Akshat Pandey <128710323+AkshatPandey2006@users.noreply.github.com>
2024-05-17 15:04:21 +0530

    Create Reverse_Integer.cpp

---
+++ Reverse_Integer.cpp
@@ -0,0 +1,13 @@
+int main() {
+    int n;
+    cin >> n;
+    int revNum = 0;
+    
+    while(n>0) {
+        int ld = n%10;
+        revNum = (revNum*10) + ld;
+        n = n/10;
+    }
+    
+    cout << revNum;
+}
