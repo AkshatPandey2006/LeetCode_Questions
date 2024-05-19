72acd96dc085eb79235a079ba12647203550fdd6
Akshat Pandey <128710323+AkshatPandey2006@users.noreply.github.com>
2024-05-19 15:31:47 +0530

    Create Insert_at_Tail.cpp

---
+++ Insert_at_Tail.cpp
@@ -0,0 +1,13 @@
+//insert at tail
+Node *insertTail(Node *head, int val) {
+    if(head == NULL) {
+        return new Node(val);
+    }
+    Node *temp = head;
+    while(temp -> next != NULL) {
+        temp = temp -> next;
+    }
+    Node *newNode = new Node(val);
+    temp -> next = newNode;
+    return head;
+}
