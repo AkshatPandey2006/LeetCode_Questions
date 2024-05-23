dc8c6d27cd0f68e2469abdef36c01e6e8a7286a5
Akshat Pandey <128710323+AkshatPandey2006@users.noreply.github.com>
2024-05-23 18:14:35 +0530

    Create Find_Longest_Chain.cpp

---
+++ Find_Longest_Chain.cpp
@@ -0,0 +1,19 @@
+static bool cmp(vector<int> &a, vector<int> &b) {
+    if(a[1] == b[1]) {
+        return a[0] < b[0];
+    }
+    return a[1] < b[1];
+}
+
+int findLongestChain(vector<vector<int>> &pairs) {
+    sort(pairs.begin(), pairs.end(), cmp);
+    int c = 0;
+    int temp = INT_MIN:
+    for(int i=0; i<pairs.size(); i++) {
+        if(temp == INT_MIN || temp < pairs[i][0]) {
+            temp = pairs[i][1];
+            c++;
+        }
+    }
+    return c;
+}
